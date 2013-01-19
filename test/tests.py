1fbb8bc65228162c0e683ab932ff0ca2540f33f0
shaung <shaun.geng@gmail.com>
2013-01-20 02:31:24 +0900

    Add tests

--- test/tests.py
+++ test/tests.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from loop import loop, Loop
+from nose.tools import eq_
 
-def test_first():
+from loops import loop, Loop
+
+iterable = xrange(8)
+
+def test_basic():
+    eq_([x for l, x in loop(iterable)], list(iterable))
+    eq_([l.index for l, x in loop(iterable)], range(len(list(iterable))))
+    eq_([l.index for l, x in loop(iterable) if not l.isfirst()], list(iterable)[1:])
+    eq_([l.index for l, x in loop(iterable) if not l.islast()], list(iterable)[:-1])
+    eq_([l.index for l, x in loop(iterable) if l.iseven()], list(iterable)[::2])
+
+def test_empty():
+    l = Loop(range(0))
+    for x in l():
+        pass
+    eq_(l.isempty(), True)
+
+def test_multiitem():
+    eq_(sum(((a, b) for l, (a, b) in loop(iterable, n=2)), ()), tuple(iterable))
+    eq_([l.index for l, x in loop(iterable, n=2)], range(len(list(iterable)))[::2])
+
+
+def test_index():
     l = Loop(range(9))
     for x in l(3):
         print l.index, l.count, l.isfirst(), l.islast(), l.iseven(), x
@@ -14,5 +36,5 @@ def test_wrapper():
         print l.index, l.count, l.isfirst(), l.islast(), l.iseven(), x
 
 if __name__ == '__main__':
-    test_first()
+    test_index()
     test_wrapper()
